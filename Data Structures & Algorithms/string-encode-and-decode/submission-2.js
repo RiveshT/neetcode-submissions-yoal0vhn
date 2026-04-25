fdab2a35dc98b1f6204715ef3567dc437408470f
Rivesh Tiwari <104714929+RiveshT@users.noreply.github.com>
2026-04-25 18:08:59 +0530

    Add: string-encode-and-decode - submission-2

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-2.js
@@ -0,0 +1,34 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    encode(strs) {
+        let encodedStr = ''
+
+        for (let str of strs) {
+            encodedStr += str.length + '#' + str
+        }
+
+        return encodedStr
+    }
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    decode(str) {
+        const strArray = []
+        let i = 0
+
+        while (i < str.length) {
+            let j = i
+            while (str[j] !== "#") j++
+            let len = parseInt(str.slice(i, j))
+            strArray.push(str.slice(j+1, j+1+len))
+            i = j+1+len
+        }
+
+        return strArray
+    }
+}
